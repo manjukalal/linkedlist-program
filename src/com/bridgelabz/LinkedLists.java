816146fadebc6bbd5fa4b3a04c9ba633d8b32a9b
manjukalal <manjukalal1496@gmail.com>
2022-06-20 20:06:13 +0530

    Added:simple linkedlist

---
+++ src/com/bridgelabz/LinkedLists.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+import java.util.LinkedList;
+public class LinkedLists {
+    public static void main(String[] args) {
+        LinkedList<Integer> Link = new LinkedList<>();
+        Link.add(56);
+        Link.add(30);
+        Link.add(70);
+        System.out.println("linkedlist is :" +Link);
+    }
+}
