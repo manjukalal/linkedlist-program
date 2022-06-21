fa448814e0bb548fd088d0629516404d2edb8a6b
manjukalal <manjukalal1496@gmail.com>
2022-06-21 23:17:35 +0530

    Added:arranging sequence in ascending order

--- src/com/bridgelabz/LinkedLists.java
+++ src/com/bridgelabz/LinkedLists.java
@@ -6,16 +6,19 @@ public class LinkedLists {
 
     public static void main(String[] args) {
 
-
         LinkedListLogic linkedListLogic = new LinkedListLogic();
         linkedListLogic.insertFirst(56);
         linkedListLogic.insertLast(70);
         linkedListLogic.insertBetween(56, 70, 30);
         linkedListLogic.toPrint();
         linkedListLogic.searcNode(56);
-        linkedListLogic.insertNumber(30, 40);
+        linkedListLogic.insertNumber(30,40);
         linkedListLogic.toPrint();
         linkedListLogic.deleteAnyNumber(40);
+        linkedListLogic.toPrint();
+        linkedListLogic.insertNumber(30,40);
+        linkedListLogic.toPrint();
+        linkedListLogic.ascendingOrder();
         linkedListLogic.toPrint();
     }
 }
