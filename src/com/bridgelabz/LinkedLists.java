4d85f82f2a61a9c326686ebfb2b8031513055512
manjukalal <manjukalal1496@gmail.com>
2022-06-21 22:27:20 +0530

    Added:inserting new data between two nodes

--- src/com/bridgelabz/LinkedLists.java
+++ src/com/bridgelabz/LinkedLists.java
@@ -3,14 +3,13 @@ package com.bridgelabz;
 import java.util.LinkedList;
 
 public class LinkedLists {
-    public static void main(String[] args) {
 
-        LinkedListLogic linkedlistlogic = new LinkedListLogic();
-        linkedlistlogic.insertNode(56);
-        linkedlistlogic.appending(56, 30);
-        linkedlistlogic.appending(30, 70);
-        linkedlistlogic.toPrint();
-    }
+  public static void main(String[] args) {
+
+    LinkedListLogic functionClass = new LinkedListLogic();
+    functionClass.insertNode(56);
+    functionClass.insertLast(70);
+    functionClass.insertBetween(56, 70, 30);
+    functionClass.toPrint();
+  }
 }
-
-
